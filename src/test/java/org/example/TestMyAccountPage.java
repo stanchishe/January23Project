6009b8696eadcc7b7d344eb7eeb586fbdc2caecc
stanc <stanko_uzunov@yahoo.com>
2024-01-17 22:36:30 +0100

    Enable users can now register new ids

---
+++ src/test/java/org/example/TestMyAccountPage.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.*;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class TestMyAccountPage {
+        public static WebDriver webDriver;
+        public static MyAccountPage myAccountPage;
+
+        @Before
+        public static void initState() {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("Start-Maximized");
+                //options.addExtensions(new File("src/resources/ChromeExtension/Adblocker2.crx"));
+                webDriver = new ChromeDriver(options);
+                webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+                myAccountPage = new MyAccountPage(webDriver);
+        }
+
+        @After
+        public static void closeState() {
+                myAccountPage.closeBrowser();
+        }
+
+        @Given("the browser window is open")
+        public void the_browser_window_is_open() {
+                Assertions.assertTrue(myAccountPage.isBrowserOpen(), "The browser is not open!");
+        }
+        @Given("the correct page is loaded")
+        public void the_correct_page_is_loaded() {
+                boolean correctPageLoaded;
+                try {
+                        myAccountPage.isLoaded();
+                        correctPageLoaded = true;
+                } catch (Error e) {
+                        correctPageLoaded = false;
+                }
+                Assertions.assertTrue(correctPageLoaded, "The correct url was not loaded!");
+        }
+        @When("^the user enters a valid email (.*) and pass (.*)$")
+        public void the_user_enters_a_valid_email_and_pass(String userEmail, String userPass) {
+                myAccountPage.enterEmail(userEmail);
+                myAccountPage.enterPass(userPass);
+        }
+        @When("clicks the registration button")
+        public void clicks_the_registration_button() {
+                myAccountPage.registerUser();
+        }
+        @Then("^a new userId (.*) is registered on the server$")
+        public void a_new_user_id_is_registered_on_the_server(String expectedUser) {
+                String registeredUser = webDriver.findElement(
+                        By.xpath("//*[@id=\"page-36\"]/div/div[1]/div/p[1]/strong")
+                ).getText().trim();
+                Assertions.assertEquals(expectedUser.split("@")[0], registeredUser, "The user was not registered correctly!");
+        }
+}
