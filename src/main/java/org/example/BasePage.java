07d0406009313ea09781e94e30b8b88678d8fd77
stanc <stanko_uzunov@yahoo.com>
2024-01-17 22:34:48 +0100

    Check if consent form is loaded and wait for scripts to load

--- src/main/java/org/example/BasePage.java
+++ src/main/java/org/example/BasePage.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.LoadableComponent;
+
+import java.util.List;
 
 public class BasePage extends LoadableComponent {
 
@@ -18,6 +21,15 @@ public class BasePage extends LoadableComponent {
         this.webDriver = webDriver;
         this.pageUrl = pageUrl;
         this.load();
+        List<WebElement> declineOptions = webDriver.findElements(By.xpath("/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[2]"));
+        if(declineOptions.size() > 0) {
+            declineOptions.get(0).click();
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         PageFactory.initElements(webDriver, this);
     }
 
